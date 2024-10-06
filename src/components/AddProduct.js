3e62e8a8b4b3574a7f6d3a305bdd38c2de1b3682
luckkalpna <luckkalpna@gmail.com>
2024-10-06 15:00:56 +0530

    Integrte Add Product API

--- src/components/AddProduct.js
+++ src/components/AddProduct.js
@@ -5,9 +5,27 @@ const AddProduct  = () =>{
     const [price, setPrice] = React.useState("");
     const [category, setCategory] = React.useState("");
     const [company, setComapny] = React.useState("");
-    const AddProduct = () =>{
+    const AddProduct = async () =>{
         console.log(name, price, category, company)
-    }
+        const userId = JSON.parse(localStorage.getItem("user"))._id;
+        // Await the fetch call
+    let result = await fetch("http://localhost:5000/add-product", {
+        method: "post",
+        body: JSON.stringify({ name, price, category, company, userId}),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      // Await the result.json() call
+      result = await result.json();
+      console.log(result);
+    //   localStorage.setItem("user", JSON.stringify(result));
+    //   if (result) {
+    //     navigate("/");
+    //   }
+    // };
+     }
     return(
         <div className="add-product">
       <h1>Add Product</h1>
